9bfc917327d18a36dc34d3437947bb28ddabdb5b
Estiven-Pelaez <grafflove2016@gmail.com>
2024-08-18 16:57:48 -0500

    primer commit a repo

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.js'],
+  darkMode: false, // o 'media' o 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
